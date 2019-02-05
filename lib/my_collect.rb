5acbd1ede6fd2ce18d8c5bcb9f479cfd59e2795b
Jason Fournier <jmf4nier@hotmail.com>
2019-02-05 22:09:03 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+array = ["1 2", "Tom Smith", "Jim Campagno"]
 
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length 
+    new_array << yield(array[i])
+    i = i + 1
+    
+  end
+  new_array
+end
+
+my_collect(array) do |name|
+   name.split(" ").last
+end
+    
 
